1bb617ead2325ac1cb966ced78ea14ac2eefdf15
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-24 21:40:04 +0200

    feat: created pagination types and interfaces for entire app

---
+++ src/utils/pagination/pagination-query.dto.ts
@@ -0,0 +1,26 @@
+import { IsIn, IsInt, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class PaginationQueryDto {
+
+  @IsOptional()
+  @IsInt()
+  @Type(() => Number)
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @IsInt()
+  @Type(() => Number)
+  @Min(1)
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsString()
+  sortBy?: string = 'id';
+
+  @IsOptional()
+  @IsIn(['ASC', 'DESC'])
+  sortOrder?: 'ASC' | 'DESC' = 'ASC'
+
+}
