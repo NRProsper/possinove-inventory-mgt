7fdce6bd6399e28b3890e39c5e4f3f3445b7f379
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-24 21:38:55 +0200

    feat: Defined product creatin dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  category: number;
+
+}
