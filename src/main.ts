0b1f8abbb74899b3668dfb4696ad77841fef14ae
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-26 23:01:00 +0200

    refactor: better api docs url, removed unnecessary tag

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true}))
-  app.setGlobalPrefix('api')
+
+  const apiPrefix = 'api'
+  app.setGlobalPrefix(apiPrefix)
 
   const config = new DocumentBuilder()
     .setTitle('Possinove Inventory Management')
     .setDescription('This is a possinove challenge to build a simple inventory management system')
     .setVersion('1.0')
-    .addTag('Possinove Inventory')
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-documentation', app, documentFactory);
+  SwaggerModule.setup(`${apiPrefix}/docs`, app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
