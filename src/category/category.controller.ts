b46f180978555fb3a0ef3ef47d5f24ae49a1232d
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-26 21:56:42 +0200

    feat: Added things related to categories

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { CategoryService } from './category.service';
 
 
 @Controller('categories')
 export class CategoryController {
-  
+
+  constructor(
+    private readonly categoryService: CategoryService
+  ) {}
+
+  @Post()
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoryService.create(createCategoryDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.categoryService.findAll();
+  }
+
 }
