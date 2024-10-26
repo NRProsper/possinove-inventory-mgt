41b3cd92d2eaf511e0f24f4848b93e7424e45a6d
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-26 22:14:53 +0200

    docs: added basic config for the swagger docs to work

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true}))
   app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder()
+    .setTitle('Possinove Inventory Management')
+    .setDescription('This is a possinove challenge to build a simple inventory management system')
+    .setVersion('1.0')
+    .addTag('Possinove Inventory')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-documentation', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
