b67015e72b3e2c430b65f828d5a24d0c12b99230
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-26 23:01:38 +0200

    docs: excluded the welcome controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,16 +3,25 @@ import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationQueryDto } from '../utils/pagination/pagination-query.dto';
+import { ApiBody, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOperation({summary: 'Create a new product'})
+  @ApiBody({type: CreateProductDto})
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  @ApiOperation({summary: 'Get all products'})
+  @ApiQuery({ name: 'page', required: false, type: Number, description: 'Page number' })
+  @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Number of items per page' })
+  @ApiQuery({ name: 'sortBy', required: false, type: String, description: 'Field to sort by' })
+  @ApiQuery({ name: 'sortOrder', required: false, enum: ['ASC', 'DESC'], description: 'Sort order' })
   @Get()
   findAll(
     @Query() paginationQuery: PaginationQueryDto
@@ -20,17 +29,20 @@ export class ProductController {
     return this.productService.findAll(paginationQuery);
   }
 
+  @ApiOperation({summary: 'Get single product by Id'})
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.productService.findOne(+id);
   }
 
+  @ApiOperation({summary: 'Update a single product'})
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOperation({summary: 'Delete a single product'})
   remove(@Param('id') id: number) {
     return this.productService.remove(+id);
   }
