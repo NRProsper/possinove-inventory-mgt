8a83bbba2b7cca43421a7a5b68fd13196ebcf719
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-26 23:11:30 +0200

    docs: documented the categories contoller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoryService } from './category.service';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 
+@ApiTags('Categories')
 @Controller('categories')
 export class CategoryController {
 
@@ -10,11 +12,14 @@ export class CategoryController {
     private readonly categoryService: CategoryService
   ) {}
 
+  @ApiOperation({summary: 'Create a new category'})
+  @ApiBody({type: CreateCategoryDto})
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
+  @ApiOperation({summary: 'Get all categories'})
   @Get()
   findAll() {
     return this.categoryService.findAll();
--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,7 +1,12 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto{
 
+  @ApiProperty({
+    type: String,
+    description: 'The category name is required'
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
