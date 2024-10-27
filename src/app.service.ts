f1a4ab6013e9cb3f680aa3486d93952f51c81948
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-27 08:33:22 +0200

    refactor: modified the welcome message to also include link to the documentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to Possinove Inventory Management system API';
+    return `
+    <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 20px;">
+        <h1>Welcome to the Possinove Inventory Management System API</h1>
+        <p>This API allows you to manage inventory efficiently. Explore the available endpoints and features to streamline your operations.</p>
+        <p>For full API documentation, visit <a href="/api/docs" style="color: blue; text-decoration: underline;">API Documentation</a>.</p>
+      </div>
+    `;
   }
 }
