e40924d0e7624162b1ab9e4f33a6aa33cea0df53
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-10-27 15:01:12 +0200

    feat: Added the remaining api methods(requests)

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, NotFoundException, Param } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Category } from './entities/category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
@@ -21,7 +21,28 @@ export class CategoryService {
   }
 
   findOne(id: number): Promise<Category> {
-    return this.categoryRepository.findOne({where: { id }})
+    return this.categoryRepository.findOne({where: { id }, relations: ['products']})
+  }
+
+  async update(id:number, name:string) {
+    const category = await this.categoryRepository.findOne({where: {id}});
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+
+    category.name = name;
+
+    return this.categoryRepository.save(category);
+  }
+
+  async delete(id:number) {
+    const category = await this.categoryRepository.findOne({where: {id}});
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+
+    return this.categoryRepository.remove(category);
+
   }
 
 
